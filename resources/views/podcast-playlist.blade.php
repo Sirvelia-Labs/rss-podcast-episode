becd7e31c78ef8e8cdee28b2af7e8d45e9a05ba9
aalleexx98 <alex92garcia2@gmail.com>
2023-05-26 12:58:31 +0200

    scrollbar and shadow

--- resources/views/podcast-playlist.blade.php
+++ resources/views/podcast-playlist.blade.php
@@ -1,6 +1,7 @@
 <p class="pb-text-green-500">Esto es una prueba</p>
 
-<div class="pb-w-full pb-bg-orange-500 pb-h-72 pb-overflow-y-scroll">
+<div
+    class="pb-w-full pb-max-h-[700px] pb-overflow-y-scroll pb-shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)] podcast-scrollbar">
     @foreach ($feed as $item)
         <div class="podcast-item">
             <h3>

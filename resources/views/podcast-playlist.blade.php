4e8896faa82b7d5c788e406244af52f0dd729038
aalleexx98 <alex92garcia2@gmail.com>
2023-06-19 10:36:19 +0200

    play button

--- resources/views/podcast-playlist.blade.php
+++ resources/views/podcast-playlist.blade.php
@@ -1,19 +1,34 @@
-<div x-cloak x-data="{ showMoreItem: null, selectedItem: 0 }"
+<div x-cloak x-data="{ showMoreItem: null, selectedItem: 0, playedItem: null }"
     class="pb-w-full pb-max-h-[700px] pb-overflow-y-scroll pb-shadow-[0_0px_60px_-15px_rgba(0,0,0,0.3)] podcast-scrollbar pb-gap-y-4 pb-flex pb-flex-col pb-p-6">
+
+
     @foreach ($feed as $item)
         <div class="pb-border pb-border-gray-200 pb-flex pb-flex-row pb-rounded-lg pb-p-6 pb-gap-x-6 pb-cursor-pointer"
             x-on:click="selectedItem = {{ $loop->index }}">
-            <div class="pb-shrink-0 pb-w-14 pb-h-14">
-                <template x-if="selectedItem === {{ $loop->index }}">
+            <div class="pb-shrink-0 pb-w-14 pb-h-14 pb-relative pb-flex pb-justify-center pb-items-center">
+                <div class="pb-w-full pb-h-full pb-rounded-md pb-flex pb-justify-center pb-items-center pb-relative">
                     <div
-                        class="pb-w-full pb-h-full pb-bg-green-400 pb-rounded-md pb-flex pb-justify-center pb-items-center">
-                        <div class="pb-rounded-full pb-w-1/2 pb-h-1/2 pb-bg-white">
+                        class="pb-rounded-full pb-w-1/2 pb-h-1/2 pb-bg-white pb-z-10 hover:pb-scale-110 pb-flex pb-justify-center pb-items-center">
+                        <template x-if="playedItem !== {{ $loop->index }}">
+                            <div class="pb-border-b-transparent pb-border-b-[8px] pb-border-t-transparent pb-border-t-[8px] pb-border-l-[12px] pb-border-l-black"
+                                x-on:click.stop="playedItem = {{ $loop->index }}, selectedItem = {{ $loop->index }}">
 
-                        </div>
+                            </div>
+                        </template>
+                        <template x-if="playedItem === {{ $loop->index }}">
+                            <div class="pb-border-black pb-border-l-4 pb-border-r-4 pb-w-1/2 pb-h-1/2"
+                                x-on:click.stop="playedItem = null">
+
+                            </div>
+                        </template>
                     </div>
-                </template>
-                <template x-if="selectedItem !== {{ $loop->index }}">
-                    <img src="{{ $item['image'] }}">
+
+                    <img src="{{ $item['image'] }}" class="pb-absolute">
+                </div>
+                <template x-if="selectedItem === {{ $loop->index }}">
+                    <div class="pb-w-2 pb-h-2 pb-bg-indigo-600 pb-rounded-full pb-absolute pb--left-4 pb-top-[24px]">
+
+                    </div>
                 </template>
             </div>
 
@@ -41,4 +56,6 @@
             </div>
         </div>
     @endforeach
+
+
 </div>
